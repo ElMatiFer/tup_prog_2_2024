a6e9ef90894f9f2b208a8121608a9847df823e50
ElMatiFer <matifer344@outlook.com>
2024-08-14 11:27:36 -0300

    .

---
+++ Guia1.1/Ejercicio1/Ej1Guia1.1ProgII/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej1Guia1._1ProgII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            string marca = tbMarca.Text;
+            int modelo = Convert.ToInt32(upModelo.Value);
+            int añoACalcular = Convert.ToInt32(upAñoCalcular.Value);
+            double valorFabricacion = Convert.ToDouble(tbValorFabrica.Text);
+            double tasaDepre = Convert.ToDouble(tbTasa.Text);
+            int vidaUtil = Convert.ToInt32(tbVida.Text);
+
+            ClassMoto miMoto = new ClassMoto(marca, modelo, valorFabricacion);
+
+            double depreLineal = miMoto.CalcularDepreciacionLineal(añoACalcular,vidaUtil);
+            double depreAnual = miMoto.CalcularDepreciacionAnual(añoACalcular,tasaDepre);
+
+            fVerResultados vMostrar = new fVerResultados();
+            
+            vMostrar.lbResultados.Items.Add(miMoto.VerDescripcion());
+
+            vMostrar.lbResultados.Items.Add($"Depreciación lineal: ${depreLineal,10:f2}");
+
+            vMostrar.lbResultados.Items.Add($"Depreciación anual:  ${depreAnual,10:f2}");
+
+            vMostrar.ShowDialog();
+        }
+    }
+}
---
+++ Guia1.1/Ejercicio1/Ej1Guia1.1ProgII/fVerResultados.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Ej1Guia1._1ProgII
+{
+    partial class fVerResultados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbResultados = new System.Windows.Forms.ListBox();
+            this.btCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(240, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resultados";
+            // 
+            // lbResultados
+            // 
+            this.lbResultados.FormattingEnabled = true;
+            this.lbResultados.Location = new System.Drawing.Point(5, 30);
+            this.lbResultados.Name = "lbResultados";
+            this.lbResultados.Size = new System.Drawing.Size(598, 95);
+            this.lbResultados.TabIndex = 1;
+            // 
+            // btCerrar
+            // 
+            this.btCerrar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCerrar.Location = new System.Drawing.Point(243, 131);
+            this.btCerrar.Name = "btCerrar";
+            this.btCerrar.Size = new System.Drawing.Size(80, 36);
+            this.btCerrar.TabIndex = 2;
+            this.btCerrar.Text = "Cerrar";
+            this.btCerrar.UseVisualStyleBackColor = true;
+            // 
+            // fVerResultados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 172);
+            this.Controls.Add(this.btCerrar);
+            this.Controls.Add(this.lbResultados);
+            this.Controls.Add(this.label1);
+            this.Name = "fVerResultados";
+            this.Text = "fVerResultados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ListBox lbResultados;
+        private System.Windows.Forms.Button btCerrar;
+    }
+}
