c1c0713a5b758ea43a3721c086acac3cabb2f4c5
ElMatiFer <matifer344@outlook.com>
2024-08-15 18:17:31 -0300

    .

---
+++ Guia1.1/Ejercicio3/Ej3Guia1.1ProgII/Models/Peaje.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3Guia1._1ProgII.Models
+{
+    internal class Peaje
+    {
+        public int PromCantVehiculosDelMes
+        {
+            get; 
+            set;
+        }
+        public int contador = 0;
+        int[] Cantidades = new int[31];
+
+        public void RegistrarResumenDelDia(int dia, int cantidadVehiculos)
+        {
+            Cantidades[dia-1] = cantidadVehiculos;
+            contador++;
+        }
+        public int MayorMovimiento()
+        {
+            int aux=0;
+            for (int i = 0; i < contador-1; i++) 
+            {
+                for (int j = i + 1; j < contador; j++)
+                {
+                    if (Cantidades[i] < Cantidades[j])
+                    {
+                        aux = j;
+                        j = i;  
+                        i = aux;
+                    }
+                }
+            }
+            return aux;
+        }
+    }
+}
